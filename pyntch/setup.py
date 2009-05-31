dc0ffcd670bef7e05c5b9eded90811a5d411acc4
yusuke.shinyama.dummy <yusuke.shinyama.dummy@bd67b606-484b-0410-afab-bba09030e0b6>
2009-05-31 22:13:22 +0000

    code analyzer -> source code analyzer

--- pyntch/setup.py
+++ pyntch/setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(name='pyntch',
       version='20090531',
-      description='Python code analyzer',
+      description='Python source code analyzer',
       license='MIT/X',
       author='Yusuke Shinyama',
       url='http://www.unixuser.org/~euske/python/pyntch/index.html',
